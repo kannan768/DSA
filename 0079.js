00d9c52cd92f5689898293d816b0e244419abf35
kannan bs <62017300+kannan768@users.noreply.github.com>
2025-05-02 19:43:43 +0530

    add

--- 0079.js
+++ 0079.js
@@ -1,3 +1,15 @@
 console.log('5' > '6');
 console.log('apple' > 'banana'); // false (because 'a' < 'b')
 console.log('cat' > 'bat');      // true (because 'c' > 'b')
+function test() {
+  console.log(arguments); 
+}
+test(1, 2, 3);  
+
+
+
+//imediate involing function
+
+(() => {
+    console.log("hello");
+})()
